cfd89ca0c6bc6dfef70dd21b2840aba47f85406a
woogyeom <ws386@rutgers.edu>
2024-02-19 18:27:27 -0500

    Get Methods in Member.java.

--- src/Member.java
+++ src/Member.java
@@ -2,6 +2,7 @@ public class Member implements Comparable<Member> {
     private Profile profile;
     private Date expire;
     private Location homeStudio;
+    private final double EXTRA_CLASS_FEE = 10.0;
 
     public Member(Profile profile, Date expire, Location homeStudio) {
         this.profile = profile;
@@ -11,6 +12,18 @@ public class Member implements Comparable<Member> {
 
     public double bill() {
 
+    }
+
+    public Profile getProfile() {
+        return profile;
+    }
+
+    public Date getExpire() {
+        return  expire;
+    }
+
+    public Location getHomeStudio() {
+        return homeStudio;
     }
 
     @Override
@@ -38,31 +51,71 @@ public class Member implements Comparable<Member> {
     } //Again writeup said we could uniquely identify a member by profile, so I'm not sure if we need to check other fields to compare
 
     public class Basic extends Member {
-        private int numClasses;
-        public Basic(Profile profile, Date expire, Location homeStudio, int numClasses) {
-            super(profile,  expire, homeStudio);
-            this.numClasses = numClasses;
+        private static final double MONTHLY_FEE = 39.99;
+        private static final int BILLING_INTERVAL = 1;
+        private static final int MAX_CLASSES = 4;
+        public Basic(Profile profile, Date expire, Location homeStudio) {
+            super(profile, expire, homeStudio);
+
+        }
+
+        public double getMonthlyFee() {
+            return MONTHLY_FEE;
+        }
+
+        public int getBillingInterval() {
+            return BILLING_INTERVAL;
+        }
+
+        public int getMaxClasses() {
+            return MAX_CLASSES;
         }
 
     }
 
     public class Family extends Member {
-        private boolean guest;
-        public Family(Profile profile, Date expire, Location homeStudio, boolean guest) {
-            super(profile,  expire, homeStudio);
-            this.guest = guest;
+        private static final double MONTHLY_FEE = 49.99;
+        private static final int BILLING_INTERVAL = 3;
+        private static final int MAX_CLASSES = Integer.MAX_VALUE;
+        public Family(Profile profile, Date expire, Location homeStudio) {
+            super(profile, expire, homeStudio);
+        }
+
+        public double getMonthlyFee() {
+            return MONTHLY_FEE;
+        }
+
+        public int getBillingInterval() {
+            return BILLING_INTERVAL;
+        }
+
+        public int getMaxClasses() {
+            return MAX_CLASSES;
         }
     }
 
     public class Premium extends Member {
-        private int guestPass;
+        private static final double MONTHLY_FEE = 59.99;
+        private static final int BILLING_INTERVAL = 12;
+        private static final int MAX_CLASSES = Integer.MAX_VALUE;
         public Premium(Profile profile, Date expire, Location homeStudio, int guestPass) {
-            super(profile,  expire, homeStudio);
-            this.guestPass = guestPass;
+            super(profile, expire, homeStudio);
+        }
+
+        public double getMonthlyFee() {
+            return MONTHLY_FEE;
+        }
+
+        public int getBillingInterval() {
+            return BILLING_INTERVAL;
+        }
+
+        public int getMaxClasses() {
+            return MAX_CLASSES;
         }
     }
 
-    public static class Profile {
+    public class Profile implements Comparable<Profile> {
         private String fname;
         private String lname;
         private Date dob;
