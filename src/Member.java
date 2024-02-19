f931ce53eb0b8efe9256c622a707289501b8de01
woogyeom <ws386@rutgers.edu>
2024-02-19 02:51:22 -0500

    Placed the Profile class within the Member class.

--- src/Member.java
+++ src/Member.java
@@ -27,7 +27,6 @@ public class Member implements Comparable<Member> {
     /**
      * Compares this object with the specified object for order.
      */
-    @Override
     public int compareTo(Member member) {
         return this.profile.compareTo(member.profile);
     } //Again writeup said we could uniquely identify a member by profile, so I'm not sure if we need to check other fields to compare
@@ -54,6 +53,81 @@ public class Member implements Comparable<Member> {
         public Premium(Profile profile, Date expire, Location homeStudio, int guestPass) {
             super(profile,  expire, homeStudio);
             this.guestPass = guestPass;
+        }
+    }
+
+    public static class Profile {
+        private String fname;
+        private String lname;
+        private Date dob;
+
+        public Profile(String fname, String lname, Date dob) {
+            this.fname = fname;
+            this.lname = lname;
+            this.dob = dob;
+        }
+
+        public String getFname() {
+            return fname;
+        }
+
+        public void setFname(String fname) {
+            this.fname = fname;
+        }
+
+        public String getLname() {
+            return lname;
+        }
+
+        public void setLname(String lname) {
+            this.lname = lname;
+        }
+
+        public Date getDob() {
+            return dob;
+        }
+
+        public void setDob(Date date) {
+            this.dob = date;
+        }
+
+        @Override
+        public String toString() {
+            return fname + ":" + lname + ":" + dob;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+
+            if (obj == null || getClass() != obj.getClass()) {
+                return false;
+            }
+
+            Profile profile = (Profile) obj;
+
+            if (this.getFname().equals(profile.getFname()) && this.getLname().equals(profile.getLname())) {
+                if (this.getDob().compareTo(profile.getDob()) == 0) {
+                    return true;
+                }
+                return false;
+            }
+            return false;
+        }
+
+        public int compareTo(Profile profile) {
+            if (this.getLname().compareToIgnoreCase(profile.getLname()) == 0) {
+                if (this.getFname().compareToIgnoreCase(profile.getFname()) == 0) {
+                    if (this.getDob().compareTo(profile.getDob()) == 0) {
+                        return 0;
+                    }
+                    return this.getDob().compareTo(profile.getDob());
+                }
+                return this.getFname().compareToIgnoreCase(profile.getFname());
+            }
+            return this.getLname().compareToIgnoreCase(profile.getLname());
         }
     }
 }
