8f528a3ffe25049d280016ee136a2b0d8ed3a269
woogyeom <woogyeom99@gmail.com>
2024-02-26 03:46:28 -0500

    Commented.

--- src/Time.java
+++ src/Time.java
@@ -13,19 +13,40 @@ public enum Time {
     private final int hour;
     private final int minute;
 
+    /**
+     * Constructs a Time object with the specified hour and minute.
+     *
+     * @param hour The hour of the class.
+     * @param minute The minute of the class.
+     */
     Time(int hour, int minute) {
         this.hour = hour;
         this.minute = minute;
     }
 
+    /**
+     * Gets the hour of the class.
+     *
+     * @return The hour of the class.
+     */
     public int getHour() {
         return hour;
     }
 
+    /**
+     * Gets the minute of the class.
+     *
+     * @return The minute of the class.
+     */
     public int getMinute() {
         return minute;
     }
 
+    /**
+     * Returns a string representation of the time in "HH:mm" format.
+     *
+     * @return A string representation of the time.
+     */
     public String toString() {
         String strHour = String.valueOf(hour);
         String strMinute = String.format("%02d", minute);
